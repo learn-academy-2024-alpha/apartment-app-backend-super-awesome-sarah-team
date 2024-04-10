92ba78fcc59ea7f4d8e9548189ebf109317907bc
Sarah Proctor <sproctor950@gmail.com>
2024-04-10 13:27:18 -0700

    :card_file_box: added apartment resource and established a has many belongs to relationship with user

---
+++ spec/requests/apartments_spec.rb
@@ -0,0 +1,175 @@
+require 'rails_helper'
+
+RSpec.describe "Apartments", type: :request do
+  let(:user) { User.create(
+      email: 'test@example.com',
+      password: 'password',
+      password_confirmation: 'password'
+    )
+  }
+
+  describe 'GET #index' do
+    it 'returns a list of apartments and http success' do
+      apartment = user.apartments.create(
+        street: 'Test Street',
+        unit: 'Test Unit',
+        city: 'Test City',
+        state: 'Test State',
+        square_footage: 1000,
+        price: '$1000',
+        bedrooms: 1,
+        bathrooms: 1,
+        pets: 'yes',
+        image: 'https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg'
+      )
+      get apartments_path
+      expect(response).to have_http_status(200)
+      expect(apartment).to be_valid
+    end
+  end
+
+  describe 'POST #create' do
+    it 'creates a valid apartment with http success' do
+      post apartments_path, params: { 
+        apartment: { 
+          street: 'Test Street',
+          unit: 'Test Unit',
+          city: 'Test City',
+          state: 'Test State',
+          square_footage: 1000,
+          price: '$1000',
+          bedrooms: 1,
+          bathrooms: 1,
+          pets: 'yes',
+          image: 'https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg',
+          user_id: user.id
+        } 
+      }
+      apartment = Apartment.where(street: 'Test Street').first
+      expect(response).to have_http_status(200)
+      expect(apartment).to be_valid
+    end
+    it 'creates a invalid apartment' do
+      post apartments_path, params: { 
+        apartment: { 
+          street: nil,
+          unit: nil,
+          city: nil,
+          state: nil,
+          square_footage: nil,
+          price: nil,
+          bedrooms: nil,
+          bathrooms: nil,
+          pets: nil,
+          image: nil,
+          user_id: nil
+        } 
+      }
+      apartment = Apartment.where(street: nil).first
+      expect(response).to have_http_status(422)
+      expect(apartment).to eq(nil)
+    end
+  end
+
+  describe 'PATCH #update' do
+    it 'makes valid updates to an existing apartment with http success' do
+      post apartments_path, params: { 
+        apartment: { 
+          street: 'Test Street',
+          unit: 'Test Unit',
+          city: 'Test City',
+          state: 'Test State',
+          square_footage: 1000,
+          price: '$1000',
+          bedrooms: 1,
+          bathrooms: 1,
+          pets: 'yes',
+          image: 'https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg',
+          user_id: user.id
+        } 
+      }
+      apartment = Apartment.where(street: 'Test Street').first
+      patch apartment_path(apartment), params: { 
+        apartment: { 
+          street: 'Updated Test Street',
+          unit: 'Updated Test Unit',
+          city: 'Updated Test City',
+          state: 'Updated Test State',
+          square_footage: 2000,
+          price: 'Updated $2000',
+          bedrooms: 2,
+          bathrooms: 2,
+          pets: 'no',
+          image: 'https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg',
+          user_id: user.id
+        } 
+      }
+
+      apartment = Apartment.where(street: 'Updated Test Street').first
+      expect(response).to have_http_status(200)
+      expect(apartment).to be_valid
+    end
+    it 'makes invalid updates to an existing apartment' do
+      post apartments_path, params: { 
+        apartment: { 
+          street: 'Test Street',
+          unit: 'Test Unit',
+          city: 'Test City',
+          state: 'Test State',
+          square_footage: 1000,
+          price: '$1000',
+          bedrooms: 1,
+          bathrooms: 1,
+          pets: 'yes',
+          image: 'https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg',
+          user_id: user.id
+        } 
+      }
+      apartment = Apartment.where(street: 'Test Street').first
+      patch apartment_path(apartment), params: { 
+        apartment: { 
+          street: nil,
+          unit: nil,
+          city: nil,
+          state: nil,
+          square_footage: nil,
+          price: nil,
+          bedrooms: nil,
+          bathrooms: nil,
+          pets: nil,
+          image: nil,
+          user_id: nil
+        } 
+      }
+      apartment = Apartment.where(street: nil).first
+      expect(response).to have_http_status(422)
+      expect(apartment).to eq(nil)
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'deletes the apartment' do
+      post apartments_path, params: { 
+        apartment: { 
+          street: 'Test Street',
+          unit: 'Test Unit',
+          city: 'Test City',
+          state: 'Test State',
+          square_footage: 1000,
+          price: '$1000',
+          bedrooms: 1,
+          bathrooms: 1,
+          pets: 'yes',
+          image: 'https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg',
+          user_id: user.id
+        } 
+      }
+      apartment = Apartment.where(street: 'Test Street').first
+      expect {
+        delete apartment_path(apartment)
+      }.to change(Apartment, :count).by(-1)
+      expect(response).to have_http_status(204)
+    end
+  end
+
+end
