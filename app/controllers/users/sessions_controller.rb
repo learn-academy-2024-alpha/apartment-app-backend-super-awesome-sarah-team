56a4499ad0acff42eb6bc9e8560515491940885d
Sarah Proctor <sproctor950@gmail.com>
2024-04-12 16:11:58 -0700

    :passport_control: changes to jti matcher revocation strategy

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,10 +3,25 @@
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
   private
-  def respond_with(resource, _opts = {})
-    render json: resource
+  def respond_with(current_user, _opts = {})
+    render json: current_user, status: :ok
   end
   def respond_to_on_destroy
-    render json: { message: "Logged out." }
+    if request.headers['Authorization'].present?
+      jwt_payload = JWT.decode(request.headers['Authorization'].split(' ').last, Rails.application.credentials.jwt_secret_key!).first
+      current_user = User.find(jwt_payload['sub'])
+    end
+  
+    if current_user
+      render json: {
+        status: 200,
+        message: 'Logged out successfully.'
+      }, status: :ok
+    else
+      render json: {
+        status: 401,
+        message: "Couldn't find an active session."
+      }, status: :unauthorized
+    end
   end
 end
